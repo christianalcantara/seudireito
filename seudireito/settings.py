954e67679ee32178abd5a4f864a84d14769d7eff
Christian Douglas <cd@lipperhub.com>
2017-11-23 05:13:53 -0300

    heroku

--- seudireito/settings.py
+++ seudireito/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from django.utils.translation import ugettext_lazy as _
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -98,7 +99,8 @@ else:
 DATABASES = {
     'default': conn
 }
-
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
